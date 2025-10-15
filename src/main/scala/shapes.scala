cc3bbdf3700d03a8f59e7ddd4e1099547751bc57
kaiakaru <119901619+kaiakaru@users.noreply.github.com>
2025-10-15 20:54:05 +0000

    first few changes

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -5,3 +5,5 @@ enum Shape derives CanEqual:
   case Rectangle(width: Int, height: Int)
   case Location(x: Int, y: Int, shape: Shape)
   // TODO add missing cases (see test fixtures)
+  case Group(shapes: Shape*)
+  case Ellipse(width: Int, height: Int)
